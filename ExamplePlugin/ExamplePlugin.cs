007a489ca8bbdd78b5979b2259ad1f4e013e500a
rozniak <roryfewell@hotmail.com>
2016-07-22 13:30:13 +0100

    Update example plugin with command code

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -1,6 +1,7 @@
 ﻿using Oddmatics.RozWorld.API.Generic;
 using Oddmatics.RozWorld.API.Server;
 using System;
+using System.Collections.Generic;
 using System.Timers;
 
 namespace ExamplePlugin
@@ -11,6 +12,7 @@ namespace ExamplePlugin
     /// This is an example plugin used to demonstrate the simplicity of developing a plugin, here, this
     /// plugin will:
     ///     > Output a startup message
+    ///     > Register a command, /hello, that outputs a message to the sender depending on who it is
     ///     > Output a broadcast message via the server every 5 seconds
     ///     
     /// You can use this plugin code as a starting point for learning to develop your own, guides will be
@@ -35,18 +37,36 @@ namespace ExamplePlugin
 
         void Server_Starting(object sender, EventArgs e)
         {
+            // Register our test command, /hello
             RwCore.Server.Logger.Out("[PLUGIN] This is an example plugin starting up!");
+            RwCore.Server.RegisterCommand("hello", CommandHello, "Test command.", "/hello");
 
-            // Initialise your stuff here
+            // Set up broadcast timer
             BroadcastTimer = new Timer(5000);
             BroadcastTimer.Elapsed += new ElapsedEventHandler(BroadcastTimer_Elapsed);
             BroadcastTimer.Enabled = true;
             BroadcastTimer.Start();
         }
 
+
         void BroadcastTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             RwCore.Server.BroadcastMessage("Hello world!");
+        }
+
+        bool CommandHello(ICommandCaller sender, IList<string> args)
+        {
+            // This is the command function, will be called whenever /hello is sent
+
+            // You can check whether the sender is the server console itself or a player like so
+            if (sender is IRwServer)
+                sender.SendMessage("Hello server operator!");
+            else
+                sender.SendMessage("Hello player!");
+
+            // More advanced stuff can be done of course, this is just a very small example
+
+            return true;
         }
     }
 }
